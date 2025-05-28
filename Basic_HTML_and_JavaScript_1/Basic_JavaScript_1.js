af09c16700964a9006af3d57e067cd4e52f34d82
belldre <drebell@outlook.com>
2025-05-28 13:24:45 -0700

    Update Basic_JavaScript_1.js

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,12 +1,14 @@
-<!DOCTYPE html>  <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World Page</title> </head>
+    <title>My Page with External JS</title>
+</head>
 <body>
-    <h1>My First JavaScript Page!</h1>
-    <p>Check your browser's console or an alert box if your script displays output.</p>
+    <h1>Check out this alert!</h1>
+    <p>This alert is coming from an external JavaScript file.</p>
 
-    <script src="Hello_World.js"></script>
+    <script src="Basic_JavaScript_1.js"></script>
 </body>
 </html>
