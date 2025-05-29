068ca241507ec9dfd694b47eb6694d2745e94b10
belldre <drebell@outlook.com>
2025-05-29 11:19:39 -0700

    BASIC HTML AND JAVASCRIPT 1 SUBMISSION ASSIGNMENT

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,16 +1,3 @@
 // This is the alert you created earlier
-alert("Hello from Basic_JavaScript_1.js!");
+alert("Guess who's getting Postered!");
 
-// This is the function My_First_Function
-function My_First_Function() {
-    var myText = "Kiss me, I'm Irish!"; 
-    
-    // Get the paragraph element by its ID
-    var irishParagraph = document.getElementById("Irish");
-    
-    // Set the text content of the paragraph
-    irishParagraph.innerHTML = myText; 
-
-    // Add the CSS class to apply the green color
-    irishParagraph.classList.add("green-text");
-}
